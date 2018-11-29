b9f269014d93d938f2c1231ca9dd0afb4b85c2f6
Greg Corbett <greg.corbett@stfc.ac.uk>
2018-11-29 09:16:21 +0000

    Remove unnecessary extra brackets

--- common.py
+++ common.py
@@ -69,9 +69,9 @@ class ESWrite(object):
     def write(self):
         """This function writes the data to elastic search"""
         self.dictionary = json.dumps(self.dictionary)
-        requests.post(("http://elasticsearch2.gridpp.rl.ac.uk:9200/"
-                       "logstash-gridtools-metrics-%s/"
-                       "metric_data/") % date,
+        requests.post("http://elasticsearch2.gridpp.rl.ac.uk:9200/"
+                      "logstash-gridtools-metrics-%s/"
+                      "metric_data/" % date,
                       data=self.dictionary)
 
 def es_check():
